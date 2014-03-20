cf8cd15208a57daed341108e54de8e3754c8d5c4
unknown <mikram.mourad@gmail.com>
2014-03-20 11:33:16 +0100

    ajout des sessions et alimentations des evaluations

--- server.js
+++ server.js
@@ -43,11 +43,12 @@ var Enseignant3 ={};
 var nbEnregistrementEval = {};
 var promotion1,promotion2,formation1,formation2,designation1,designation2,ue1,ue2,etat1,etat2,periode1,periode2;
 var ordreRubrique1,ordreRubrique2,ordreRubrique3,designationRubrique1,designationRubrique2,designationRubrique3;
+//var ENS_NOM = "'Saliou'";
+var requetteListeEvaluations = 'SELECT * from v_evaluation where ENS_NOM =';
+//var countEvaluationEnseignant = 'SELECT count(*) as nb from v_evaluation where ENS_NOM ='+ENS_NOM;
+var requetteRubrique = "'SELECT * from v_rubeval where ENS_NOM ='Saliou'";
+var listeFormation = [];
 var listePromotion = [];
-var ENS_NOM = "'Saliou'";
-var requetteListeEvaluations = 'SELECT * from v_evaluation where ENS_NOM ='+ENS_NOM;
-var countEvaluationEnseignant = 'SELECT count(*) as nb from v_evaluation where ENS_NOM ='+ENS_NOM;
-var requetteRubrique = 'SELECT * from v_rubeval where ENS_NOM =' +ENS_NOM;
 
 connection.query('CREATE DATABASE IF NOT EXISTS evaespi', function (err) {
     if (err) throw err;
@@ -56,15 +57,18 @@ connection.query('CREATE DATABASE IF NOT EXISTS evaespi', function (err) {
         
 			
 			
-    connection.query("SELECT * from ENSEIGNANT", function(err, rows){
+    /*connection.query("SELECT * from ENSEIGNANT", function(err, rows){
         // There was a error or not?
         if(err != null) {
             res.end("Query error:" + err);
         } else {
 		
-       	
+       	var data1 = {
+					   listeEnseignants: rows
+				}
+				res.render('login.hbs',data1);
 		
-        Enseignant1= rows[0].PRENOM+' '+rows[0].NOM;
+        /*Enseignant1= rows[0].PRENOM+' '+rows[0].NOM;
 		Enseignant2= rows[1].PRENOM+' '+rows[1].NOM;
 		Enseignant3= rows[2].PRENOM+' '+rows[2].NOM;
 		Enseignant4= rows[3].PRENOM+' '+rows[3].NOM;
@@ -73,7 +77,8 @@ connection.query('CREATE DATABASE IF NOT EXISTS evaespi', function (err) {
         }
 		
 		
-    });
+    });*/
+	
 	/*connection.query(countEvaluationEnseignant, function(err, rows){
         // There was a error or not?
         if(err != null) {
@@ -108,13 +113,6 @@ connection.query('CREATE DATABASE IF NOT EXISTS evaespi', function (err) {
     });*/
 });
 
-// Database setup
-
-
-
-
-
-
 app.get('/handlebars', eval.testerHandlebars);
 app.get('/eval/edit/:titre', eval.NouvelleEvaluationParams);
 
@@ -139,119 +137,86 @@ connection.query("SELECT * from v_rubeval where EVE_ID_EVALUATION ="+id, functio
 
 });
 
+app.get('/',function(req, res) {
+	connection.query("SELECT * from ENSEIGNANT", function(err, rows){
+	
+        // There was a error or not?
+			if(err != null) {
+				res.end("Query error:" + err);
+			} else {
+			  
+				var data = {
+					   a: rows[0].NOM,
+					   listeEnseignants: rows
+				}
+				res.render('login.hbs',data);
+			}
+				
+		});	
+});
+
 
 
 app.post('/eval/editEvaluation/', eval.NouvelleEvaluationData);
 app.get('/eval/liste', evalDao.listeEvaluation);
 app.get('/index', accueil.index);
-
-
-app.get('/', function(req, res) {
-
-var data = {
-        Enseignant1: Enseignant1,
-		Enseignant2: Enseignant2,
-		Enseignant3: Enseignant3,
-		Enseignant4: Enseignant4
-       
-    }
-
-
-    res.render('login.hbs',data);
-	
-	
-});
-
-
-
 app.get('/eval/listeRubrique/', eval.listeRubrique);
 app.get('/eval/listeRubriqueEvaluation/', eval.listeRubriqueEvaluation);
 //app.post('/eval/editRubrique/', eval.NouvelleRubrique);
 app.get('/eval/injecterEvaluation', eval.InjecterNouvelleEvaluation);
-app.get('/ajouterEval', eval.ajouterEval);
-
-/**
- * Cette fonction récupère la liste des évaluations et alimente un template handlebars
- */
-app.get('/listeEval', function (req, res){
-
-			
-	connection.query(requetteListeEvaluations, function(err, rows){
+app.get('/ajouterEval', function(req, res){
+	connection.query("Select ANNEE_PRO from Promotion", function(err, rows){
         // There was a error or not?
 			if(err != null) {
 				res.end("Query error:" + err);
 			} else {
-			   /*if (nbEnregistrementEval == 2 ){ 
-					promotion1 = rows[0].EVE_ANNEE_PRO;
-					promotion2 = rows[1].EVE_ANNEE_PRO;
-					formation1 = rows[0].FRM_CODE_FORMATION;
-					formation2 = rows[1].FRM_CODE_FORMATION;
-					designation1 = rows[0].EVE_DESIGNATION;
-					designation2 = rows[1].EVE_DESIGNATION;
-					ue1 = rows[0].UE_DESIGNATION;
-					ue2= rows[1].UE_DESIGNATION;
-					etat1 = rows[0].EVE_ETAT;
-					etat2 = rows[1].EVE_ETAT;
-					periode1 = rows[0].EVE_PERIODE;
-					periode2 = rows[1].EVE_PERIODE;
-					}
-					
-					else {
-					
-					promotion1 = rows[0].EVE_ANNEE_PRO;
-					
-					formation1 = rows[0].FRM_CODE_FORMATION;
-					designation1 = rows[0].EVE_DESIGNATION;
-					ue1 = rows[0].UE_DESIGNATION;
-					etat1 = rows[0].EVE_ETAT;
-					periode1 = rows[0].EVE_PERIODE;
-					
-				}*/
-				var data1 = {
-					   listeEvaluations: rows
+			 listePromotion = rows;
+			 
+			}
+		});	
+			connection.query("Select CODE_FORMATION from Formation", function(err, rows){
+        // There was a error or not?
+			if(err != null) {
+				res.end("Query error:" + err);
+			} else {
+			listeFormation = rows;				
+			}
+		});	
+		    connection.query("Select CODE_UE from UNITE_ENSEIGNEMENT", function(err, rows){
+        // There was a error or not?
+			if(err != null) {
+				res.end("Query error:" + err);
+			} else {
+			 var data = {
+			    listePromotions: listePromotion,
+			    listeFormations: listeFormation,
+				uniteEnseignements: rows
+						}
+						console.log("ok");
+				res.render('ajouter-evaluation.hbs',data);
+			}
+		});	
+});
+
+/**
+ * Cette fonction récupère la liste des évaluations et alimente un template handlebars
+ */
+app.post('/listeEval', function (req, res){
+//var NOEnseignant = req.params.select01;
+var NOM = req.body.select01;
+
+console.log(requetteListeEvaluations+NOM);		
+	connection.query("SELECT * from v_evaluation where ENS_NOM ='"+NOM+"'", function(err, rows){
+        // There was a error or not?
+			if(err != null) {
+				res.end("Query error:" + err);
+			} else {
+			var data1 = {
+			listeEvaluations: rows
 				}
 				res.render('index-2.hbs',data1);
 			}
-				
-		});		
-			
-			
-	
-
-console.log(req.params.nom);
-if (nbEnregistrementEval == 1) {
-		
-	   var hidden = "hidden";
-		}
-		
-var data1 = {
-       // Enseignant1: Enseignant1,
-	   
-		    promotion1: promotion1,
-			promotion2: promotion2,
-			formation1: formation1,
-			formation2: formation2,
-			designation1: designation1,
-			designation2: designation2,
-			ue1: ue1,
-			ue2: ue2,
-			etat1: etat1,
-			etat2: etat2,
-			periode1: periode1,
-			periode2: periode2,
-            hidden: hidden,
-			ordreRubrique1: ordreRubrique1,
-			ordreRubrique2: ordreRubrique2,
-			ordreRubrique3: ordreRubrique3,
-			designationRubrique1: designationRubrique1,
-			designationRubrique2: designationRubrique2,
-			designationRubrique3: designationRubrique3
-		
-		
-    }
-
-
-    
+		});	
 });
 
 
