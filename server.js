efc961313a17a9eb709231f8c24dfd93843b1eca
unknown <mikram.mourad@gmail.com>
2014-03-21 10:46:31 +0100

    modification mineur

--- server.js
+++ server.js
@@ -284,10 +284,10 @@ app.post('/evalajoute', function (req, res) {
  */
 app.post('/listeEval', function (req, res){
 //var NOEnseignant = req.params.select01;
-var NumEns = req.body.select01;
+var NOM = req.body.select01;
 
-console.log(requetteListeEvaluations+NumEns);		
-	connection.query("SELECT * from v_evaluation where ENS_NOM ='"+NumEns+"'", function(err, rows){
+console.log(requetteListeEvaluations+NOM);		
+	connection.query("SELECT * from v_evaluation where ENS_NOM ='"+NOM+"'", function(err, rows){
         // There was a error or not?
 			if(err != null) {
 				res.end("Query error:" + err);
